3fbc63064446e0d501602c4af94fdbd31e822a09
Barorod <128448506+barorod@users.noreply.github.com>
2024-01-20 23:35:14 +0800

    Fixed typo

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const helmet = require('helmet');
 
 const mongoSanitize = require('express-mongo-sanitize');
 
-const dbUrl = prcoess.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl);
 
